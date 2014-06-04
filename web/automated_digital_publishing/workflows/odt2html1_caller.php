ac01004712112b400552d91319b4732f24e71d51
Stephan Kreutzer <skreutzer@fsfe.org>
2014-06-05 01:11:35 +0200

    Use odt2html1 workflow.

--- web/automated_digital_publishing/workflows/odt2html1_caller.php
+++ web/automated_digital_publishing/workflows/odt2html1_caller.php
@@ -16,7 +16,7 @@
  * along with automated_digital_publishing_server. If not, see <http://www.gnu.org/licenses/>.
  */
 /**
- * @file $/web/automated_digital_publishing/odt2html/odt2html1/caller.php
+ * @file $/web/automated_digital_publishing/workflows/odt2html1_caller.php
  * @author Stephan Kreutzer
  * @since 2014-05-31
  */
@@ -25,12 +25,32 @@
 
 if (isset($_SESSION['input']) === true)
 {
+    $locked = false;
+
+    if (file_exists(dirname(__FILE__)."/locked.txt") === true)
+    {
+        return -1;
+    }
+    else
+    {
+        file_put_contents(dirname(__FILE__)."/locked.txt", "locked");
+        $locked = true;
+    }
+
     $id = md5(uniqid(rand(), true));
     $_SESSION['output'] = $id;
 
-    $result = shell_exec("java -classpath ".dirname(__FILE__)." odt2html1 ".dirname(__FILE__)."/../../../input/".$_SESSION['input']." ".dirname(__FILE__)."/../../../output/".$id.".html 2>&1");
+    $result = shell_exec("java -classpath ".dirname(__FILE__)." odt2html1 ".dirname(__FILE__)."/../../input/".$_SESSION['input']." 2>&1");
 
-    file_put_contents(dirname(__FILE__)."/../../../output/".$id.".log", $result);
+    file_put_contents(dirname(__FILE__)."/../../output/".$id.".log", $result);
+
+    copy(dirname(__FILE__)."/temp/output_4.html", dirname(__FILE__)."/../../output/".$id.".html");
+  
+    if ($locked === true)
+    {
+        unlink(dirname(__FILE__)."/locked.txt");
+    }
+    
     return $result;
 }
 
